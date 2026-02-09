49d1e21e1a2e0ac201b8b235fb8da3c809e279b5
Quiincy <mishabelous11@gmail.com>
2026-02-09 22:47:05 +0200

    CORS added

--- backend/main.py
+++ backend/main.py
@@ -25,10 +25,14 @@ models.Base.metadata.create_all(bind=engine)
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://localhost:3000",
+        "https://svit-valut-front.onrender.com",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=[*""],
+    allow_headers=["*"],
 )
 
 # ============== SITE SETTINGS ==============
